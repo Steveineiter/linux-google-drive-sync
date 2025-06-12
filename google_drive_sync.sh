c1acd248780d3ab0cdc44f84bd042af04c991ea3
Stefan Schörkmeier <S.Schoerkmeier@student.tugraz.at>
2025-06-12 13:41:01 +0200

    changed script

--- google_drive_sync.sh
+++ google_drive_sync.sh
@@ -4,6 +4,6 @@ readonly LOCAL_DESTINATION="$HOME/GoogleDrive"
 # To try it out:
 # rclone sync --dry-run "$RSYNC_CONFIG":/ "$LOCAL_DESTINATION" --log-file=$HOME/.config/rclone/rclone.log --verbose
 # To solve cache error:
-# rclone bisync "$RSYNC_CONFIG":/ "$LOCAL_DESTINATION" --log-file=$HOME/.config/rclone/rclone.log --verbose --resync
+# rclone bisync "$RSYNC_CONFIG":/ "$LOCAL_DESTINATION" --log-file=$HOME/.config/rclone/rclone.log --verbose --resync --drive-acknowledge-abuse
 
 rclone bisync "$RSYNC_CONFIG":/ "$LOCAL_DESTINATION" --log-file=$HOME/.config/rclone/rclone.log --verbose
